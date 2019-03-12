f3741b82406a98fcf812e965ef0d069260dc9019
Cameron MacLeod <cmacleod170@googlemail.com>
2019-03-12 14:39:56 +0000

    Controller endpoints (#35)
    * Implement /controllerregister + tests
    
    * Remove dead code
    
    * Add game_id field for controller
    
    * Implement /controllerpoll + tests
    
    * Update last_seen
    
    * Fix overwriting of game_id on register
    
    Whenever the controller registered, it would overwrite any game_id that
    was already there. This fixes that and adds a regression test.
    
    * Address PR comments

--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 import os
 import json
+import time
 import logging
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from flask_socketio import SocketIO, join_room
 from schemas.game import MakeMoveInput, CreateGameInput, JoinGameInput, DrawOfferInput, RespondOfferInput, ResignInput
-from schemas.controller import ControllerRegisterInput
+from schemas.controller import ControllerRegisterInput, ControllerPollInput
 from .game import Game
 import google.cloud
 from google.cloud import firestore
@@ -30,6 +31,7 @@ else:
     db = None
 
 GAMES_COLLECTION = "games"
+CONTROLLER_COLLECTION = "controllers"
 
 BAD_REQUEST = 400
 REQUEST_OK = 'OK'
@@ -118,14 +120,53 @@ def join_game():
 
 @app.route('/controllerregister', methods=["POST"])
 def register_controller():
-    errors = ControllerRegisterInput().validate(request.form)
+    errors = ControllerRegisterInput(db).validate(request.form)
     if errors:
         abort(BAD_REQUEST, str(errors))
-    return '0'
+    controller_id = request.form["board_id"]
+    controller_ref = db.collection(CONTROLLER_COLLECTION).document(controller_id)
 
-@app.route('/controllerpoll/<board_id>')
-def controller_poll(board_id):
-    return get_game(board_id)
+    controller_dict = {"game_id": None, **request.form}
+    # avoid overwriting game_id
+    if controller_ref.get().exists:
+        controller_dict = controller_ref.get().to_dict()
+    controller_dict['last_seen'] = time.time()
+
+    controller_ref.set(controller_dict)
+    return 'registered'
+
+@app.route('/controllerpoll', methods=["POST"])
+def controller_poll():
+    errors = ControllerPollInput(db).validate(request.form)
+    if errors:
+        abort(BAD_REQUEST, str(errors))
+    controller_id = request.form['board_id']
+    controller_ref = db.collection(CONTROLLER_COLLECTION).document(controller_id)
+    controller_dict = controller_ref.get().to_dict()
+
+    # update last_seen
+    controller_dict['last_seen'] = time.time()
+    controller_ref.set(controller_dict)
+
+    poll_response = {'game_over': {'game_over': False, 'reason': None}, 'history': []}
+
+    game_id = controller_dict['game_id']
+    ply_count = int(request.form['ply_count'])
+    error = int(request.form.get('error', -1))
+
+    if error != -1:
+        # let web app know about error
+        socketio.emit("controller_error", error, room=game_id)
+
+    if game_id is not None and error == -1:
+        game_dict = db.collection(GAMES_COLLECTION).document(game_id).get().to_dict()
+        poll_response['game_over'] = game_dict['game_over']
+
+        # game_dict['ply_count'] == len(history)
+        for i in range(ply_count, game_dict['ply_count']):
+            poll_response['history'].append(game_dict['history'][i])
+
+    return jsonify(poll_response)
 
 @socketio.on('register')
 def register_for_game_updates(game_id):
--- test/routes/test_controller_register.py
+++ test/routes/test_controller_register.py
@@ -1,7 +1,14 @@
 """Test cases for the POST server route /controllerregister."""
 
+import time
 import unittest
+from unittest.mock import patch
 from server.server import app
+from server.schemas.controller import CONTROLLER_COLLECTION, TIMEOUT
+from .mock_firebase import MockClient
+
+OK = 200
+BAD_REQUEST = 400
 
 class ControllerRegisterTest(unittest.TestCase):
     # Setup and helper functions
@@ -16,9 +23,67 @@ class ControllerRegisterTest(unittest.TestCase):
         """Helper function for making POST requests.
 
         Usage:
-            # POST /controllerregister?param_1=1&param_2=2
+            # POST /controllerregister -F "param_1=1&param_2=2"
             response = self.post({param_1: 1, param_2: 2})
         """
-        return ControllerRegisterTest.client.post(ControllerRegisterTest.route, data=data)
+        return self.client.post(self.route, data=data)
 
-    # TODO: Tests
+    @patch('server.server.db', new_callable=MockClient)
+    def test_new_controller_can_register(self, mock_db):
+        "Tests that a controller can register if not seen before"
+        controller_id = "kevin"
+        params = {"board_id": controller_id,
+                  "board_version": "0.0.1"}
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+        # assert that controller is in database
+        self.assertTrue(mock_db.collection(CONTROLLER_COLLECTION).document(controller_id).exists)
+
+    @patch('server.server.db', new_callable=MockClient)
+    def test_controller_cant_register_twice(self, mock_db):
+        "Tests that a controller can't register within the time-out window"
+        params = {"board_id": "kevin",
+                  "board_version": "0.0.1"}
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+        response = self.post(params)
+        self.assertEqual(BAD_REQUEST, response.status_code)
+
+    @patch('server.server.db', new_callable=MockClient)
+    def test_controller_can_re_register_after_time_out(self, mock_db):
+        "Test controller can re-register after time-out"
+        controller_id = "kevin"
+        params = {"board_id": controller_id,
+                  "board_version": "0.0.1"}
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+        # make sure at least TIMEOUT seconds have "passed"
+        mock_db.collection(CONTROLLER_COLLECTION).document(controller_id).data["last_seen"] -= 2*TIMEOUT
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+
+    @patch('server.server.db', new_callable=MockClient)
+    def test_controller_register_with_no_game(self, mock_db):
+        "When a controller registers it should not have a game assigned"
+        controller_id = "kevin"
+        params = {"board_id": controller_id,
+                  "board_version": "0.0.1"}
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+        self.assertIsNone(mock_db.collection(CONTROLLER_COLLECTION).document(controller_id).data["game_id"])
+
+    @patch('server.server.db', new_callable=MockClient)
+    def test_register_does_not_overwrite_game_id(self, mock_db):
+        "Regression for a bug where registering overwrote existing game_id"
+        controller_id = "kevin"
+        game_id = "some_id"
+        params = {"board_id": controller_id,
+                  "board_version": "0.0.1"}
+        c_w_game_id = {"board_id": controller_id, "board_version": "0.0.1",
+                       "game_id": game_id, "last_seen": time.time() - 6000} # 6000 >> TIMEOUT
+
+        mock_db.collection(CONTROLLER_COLLECTION).document(controller_id).set(c_w_game_id)
+        response = self.post(params)
+        self.assertEqual(OK, response.status_code)
+        self.assertEqual(game_id, mock_db.collection(
+            CONTROLLER_COLLECTION).document(controller_id).data['game_id'])
